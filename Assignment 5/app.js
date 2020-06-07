ffe65e1da9114105d12d454238208d5617dd2215
Sumeet Kumar <sumeet.kumar38@yahoo.com>
2020-06-07 16:56:56 +0500

    Done Chapter 9-11

--- Assignment 5/app.js
+++ Assignment 5/app.js
@@ -335,8 +335,6 @@ document.write('<p>Estimated Maximum Age: ' + maxAge + '</p>')
 document.write('<p>Amount of snacks per day: ' + amount + '</p>')
 document.write('<p>You will need ' + lifetime + ' to last you until the ripe old age of ' + maxAge + '</p>')
 
-*/
-
 // ------------------------ End of Chapter 5 ------------------------ //
 
 
@@ -471,4 +469,204 @@ document.write('</tr>')
 
 document.write('</table>')
 
+*/
+
 // ------------------------ End of Chapter 6-9 ------------------------ //
+
+
+// ------------------------ Chapter 9 - 11 : User Input and Conditional Statement ------------------------ //
+
+//Q1
+var city = prompt('Please enter City Name:')
+if (city.toLowerCase() === 'karachi'){
+    alert('Welcome to city of lights')
+}
+
+//Q2
+var gender = prompt('Enter Your Gender')
+if (gender.toLowerCase() === 'male'){
+    alert('Good Morning Sir')
+}
+else{
+    alert('Good Morning Maam')
+}
+
+//Q3
+var color = prompt('Enter the color of traffic road signal: \n Red, Yellow or Green')
+if (color.toLowerCase() === 'red'){
+    alert('Must Stop')
+} 
+if (color.toLowerCase() === 'yellow'){
+    alert('Ready to Move')
+} 
+if (color.toLowerCase() === 'green'){
+    alert('Move Now')
+} 
+
+//Q4
+var fuel = prompt('Enter your remaining fuel (in liters):')
+if (fuel < 0.25){
+    alert('Please refill your fuel in car')
+}
+
+
+//Q5
+
+// Q5a.
+var a = 4 
+if (++a === 5){ 
+    alert('given condition for variable a is true')
+    }
+
+// Q5b.
+var b = 82
+if (b++ === 83){
+    alert('given condition for variable b is true')
+    }
+
+// Q5c.
+var c = 12
+if (c++ === 13){
+    alert('condition 1 is true')
+    }
+
+if (c === 13){ 
+    alert('condition 2 is true') 
+    }
+
+if (++c < 14){ 
+    alert('condition 3 is true') 
+    } 
+
+if(c === 14){ 
+    alert('condition 4 is true') 
+    }
+
+// Q5d.
+var materialCost = 20000 
+var laborCost = 2000 
+var totalCost = materialCost + laborCost 
+if (totalCost === laborCost + materialCost){ 
+    alert('The cost equals') 
+    }
+
+// Q5e.
+if (true){ 
+    alert('True') 
+    } 
+
+if (false){ 
+    alert('False') 
+    }
+
+// Q5f.
+if('car' < 'cat'){ 
+    alert('car is smaller than cat') 
+    }
+
+// Q6
+var obtainedMarks = parseFloat(prompt('Enter Obtained Marks in three subjects:'))
+var totalMarks = parseInt(prompt('Enter total Marks:'))
+var percentage = (obtainedMarks / totalMarks) * 100
+
+document.write('<h3>' + 'Mark Sheet' + '<h3>')
+document.write('Total Marks: ' + totalMarks + '</br>')
+document.write('Marks Obtained: ' + obtainedMarks + '</br>')
+document.write('Percentage: ' + percentage + '</br>')
+
+if (percentage >= 80 && percentage < 100){
+    document.write('Grade: A-one' + '<br>')
+    document.write('Remarks: Excellent' + '<br>')
+}
+
+else if (percentage >= 70 && percentage < 80){
+    document.write('Grade: A' + '<br>')
+    document.write('Remarks: Good' + '<br>')
+}
+
+else if (percentage >= 60 && percentage < 70){
+    document.write('Grade: B' + '<br>')
+    document.write('Remarks: You need to improve' + '<br>')
+}
+
+else if (percentage < 60){
+    document.write('Grade: Fail' + '<br>')
+    document.write('Remarks: Sorry' + '<br>')
+}
+else{
+    document.write('Invalid Input' + '<br>')
+}
+
+// Q7
+var secret = 2
+var guess = parseInt(prompt('Guess the Number: \nBetween 1-10'))
+if (secret === guess){
+    alert('Bingo! Correct Answer: ')
+}
+else if (secret === guess+1){
+    alert('Close enough to the correct answer')
+}
+else{
+    alert('Sorry Wrong Guess')
+}
+
+//Q8
+var num = parseInt(prompt('Enter number to check number is divisible by 3 or not: '))
+if (num % 3 === 0){
+    alert(num + ' is divisble by 3')
+}
+else{
+    alert(num + ' is not divisible by 3')
+}
+
+//Q9
+var num = parseInt(prompt('Enter number to check number is even or odd'))
+if (num % 2 === 0){
+    alert(num + ' is even number')
+}
+else{
+    alert(num + ' is odd number')
+}
+
+//Q10
+var temperature = +prompt('Enter your city temperature: ')
+if (temperature > 40){
+    alert('It is too hot outside')
+}
+else if (temperature > 30){
+    alert('The weather today is Normal')
+}
+else if(temperature > 20){
+    alert('Today\'s Weather is cool')
+}
+else{
+    alert('OMG! Today\'s weather is so Cool')
+}
+
+// Q11
+var first = parseInt(prompt('Enter First Number: '))
+var second = parseInt(prompt('Enter Second Number: '))
+var operator = prompt('Choose your operator: \n( + , - , * , / , % )')
+document.write('<h3>' + 'Calculator' + '</h3>')
+document.write('First Number: ' + first + '</br>')
+document.write('Second Number: ' + second + '</br>')
+if (operator === '+'){
+    document.write('Result:'+first+'+'+second+'='+(first + second))
+}
+else if (operator === '-'){
+    document.write('Result:'+second+'-'+first+'='+(second - first))
+}
+else if (operator === '*'){
+    document.write('Result:'+first+'x'+second+'='+(first * second))
+}
+else if (operator === '/'){
+    document.write('Result:'+first+'/'+second+'='+(first / second))
+}
+else if (operator === '%'){
+    document.write('Result:'+first+'%'+second+'remainderis:'+(first % second))
+}
+else{
+    document.write('Wrong Input')
+}
+
+// ------------------------ End of Chapter 9 - 11 ------------------------ //
