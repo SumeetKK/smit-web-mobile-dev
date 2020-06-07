5e1a9370668b9c771a9d307cf7db3e3628439556
Sumeet Kumar <sumeet.kumar38@yahoo.com>
2020-06-07 14:26:25 +0500

    Completed Chapter 2

--- Assignment 5/app.js
+++ Assignment 5/app.js
@@ -1,5 +1,6 @@
 // ------------------------ Chapter 1 : Alerts ------------------------ //
 
+/*
 //Q1.
 alert('Hello Visitor')
 
@@ -15,6 +16,52 @@ alert('Happy Coding!');
 
 //Q5
 console.log('Yes I can generate alert using developer tools\' console, but I cannot write code for it')
-
+*/
 
 // ------------------------ End of Chapter 1 ------------------------ //
+
+
+
+// ------------------------ Chapter 2 : Variables for String ------------------------ //
+
+//Q1
+var username;
+
+//Q2
+var myName = 'Sumeet Kumar';
+
+//Q3
+var message;
+message = 'Hello World';
+alert(message);
+
+//Q4
+var name = prompt('Please enter your name:');
+var age = prompt('Please enter your age:');
+var course = prompt('Please enter your course name:');
+alert(name);
+alert(age+" years old");
+alert(course);
+
+
+//Q5
+var pizza = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+alert(pizza);
+
+//Q6
+var email = 'sumeet.kumar38@yahoo.com';
+alert('My email address is ' +email);
+
+//Q7
+var book = 'A smarter way to learn JavaScript';
+alert('I am trying to learn from the Book ' +book);
+
+//Q8
+document.write('Yah! I can write HTML content through JavaScript');
+
+//Q9
+var line = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+alert(line);
+
+
+// ------------------------ End of Chapter 2 ------------------------ //
