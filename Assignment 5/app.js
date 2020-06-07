6f890ff31b82878af861512698771c7996161bab
Sumeet Kumar <sumeet.kumar38@yahoo.com>
2020-06-07 14:15:50 +0500

    Chapter 1 Done

---
+++ Assignment 5/app.js
@@ -0,0 +1,20 @@
+// ------------------------ Chapter 1 : Alerts ------------------------ //
+
+//Q1.
+alert('Hello Visitor')
+
+//Q2
+alert('Error! Please Enter A Valid Password.')
+
+//Q3
+alert('Welcome to JS Land... \nHappy Coding!');
+
+//Q4
+alert('Welcome to JS Land...');
+alert('Happy Coding!');
+
+//Q5
+console.log('Yes I can generate alert using developer tools\' console, but I cannot write code for it')
+
+
+// ------------------------ End of Chapter 1 ------------------------ //
